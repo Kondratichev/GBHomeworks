f07f20cebb1cff60164a254bd3a17f4b410c8c15
Kondratichev <kondratichev@icloud.com>
2020-07-08 21:39:03 +0300

    String to Array

--- src/main/java/homeworkthree/WorkThreeEx2.java
+++ src/main/java/homeworkthree/WorkThreeEx2.java
@@ -1,5 +1,6 @@
 package homeworkthree;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class WorkThreeEx2 {
@@ -18,14 +19,20 @@ public class WorkThreeEx2 {
         String askWord = new String();
         int rand = (int) (Math.random() * arr.length);
         askWord = arr[rand];                             // Задаем рандомное значение проверочному слову
-        String tip = new String();                      // Переменная для вывода подсказки
+        //String tip = new String();
+        char [] tip = new char[15];
+        for (int i = 0; i <15; i++) {           //Работать посимвольно со строкой ц меня не получилось.
+                                            // Преобразую подсказку в массив... и заполним его
+            tip[i] = '#';
+        }
+        //tip = "###############";                                      // Переменная для вывода подсказки
         System.out.println("Назови слово (фрукт или овощ) и нажми Enter");
         int lengthOfWord;   // не очень хочется вводить еще одну переменную, но если не сравнить длину строк для
         // работы цикла - попадаешь на исключение, если длина введенной строки меньше длины загаданного слова
 
         while (check) {
             String input_word = scan.nextLine();
-            tip = "";               // Каждый раз будем обнулять подсказку
+           // tip = "";               // Каждый раз будем обнулять подсказку
             if (askWord.length() > input_word.length()) {   // Проверяю, какое из слов короче, что б взять его длину за основу в цикле и не попасть на исключение
                 lengthOfWord = input_word.length();
             } else lengthOfWord = askWord.length();
@@ -39,16 +46,23 @@ public class WorkThreeEx2 {
                                                                     // сравниваем их значение
 
                     if (askWord.charAt(i) == input_word.charAt(i)) {
-                        tip = tip + askWord.charAt(i);      //приклеиваем символ к строке, если он совпадает
-                    } else tip = tip + '#';                     // и приклеиваем * - если не совпадает
+                        //tip = tip + askWord.charAt(i);
+                        tip[i] = askWord.charAt(i);
+                                                                    // приклеиваем символ к строке, если он совпадает
+                    } //else if (tip[i] !=  )
+                     //   tip = tip + '#';                     // и приклеиваем * - если не совпадает
                 }
-                for (int k = tip.length(); k < 15; k++) {       //Не очень нужный цикл, который приклеивает # - что бы
+                //for (int k = tip.length(); k < 15; k++) {       //Не очень нужный цикл, который приклеивает # - что бы
                                                                 // получилось ровно 15 символов в подсказке
-                    tip = tip + '#';
-                }
+                 //   tip = tip + '#';
+             //   }
 
             if (check) {
-                System.out.println("Вот какие буквы ты угадал в загаданном слове - " + tip + "\n Попробуй снова");
+                System.out.print("Вот какие буквы ты угадал в загаданном слове - ");
+                for (int i = 0; i <15 ; i++) {
+                    System.out.print(tip[i]);
+                }
+                System.out.println("\n Попробуй снова");
             }
         }
     }
